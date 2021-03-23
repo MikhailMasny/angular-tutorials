f19d553cb64f49f20ef8da84ba908fbcc7f2a93f
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2021-03-23 22:20:51 +0300

    feat: add basic implementation of auth system

--- angular-jwt/src/app/_services/auth.service.ts
+++ angular-jwt/src/app/_services/auth.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const AUTH_API = 'https://localhost:5001/api/users/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'signin', {
+      username, 
+      password
+    }, httpOptions);
+  }
+
+  register(email: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'signup', {
+      email,
+      password
+    }, httpOptions);
+  }
 }
--- angular-jwt/src/app/register/register.component.ts
+++ angular-jwt/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  form: any = {
+    username: null,
+    email: null,
+    password: null
+  };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    const { email, password } = this.form;
+
+    this.authService.register(email, password).subscribe(
+      data => {
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
 }
